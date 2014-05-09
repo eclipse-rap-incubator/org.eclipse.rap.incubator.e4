6c19273a5b4744f41ea5d362e32365fad2f25694
Tom Schindl <tom.schindl@bestsolution.at>
2014-05-09 09:36:09 +0200

    Bug 434473 - [e4] Sync forked bundles with upstream

--- bundles/org.eclipse.e4.ui.bindings/src/org/eclipse/e4/ui/bindings/keys/KeyBindingDispatcher.java
+++ bundles/org.eclipse.e4.ui.bindings/src/org/eclipse/e4/ui/bindings/keys/KeyBindingDispatcher.java
@@ -77,6 +77,7 @@ public class KeyBindingDispatcher {
 		 * @param event
 		 *            The event to process; must not be <code>null</code>.
 		 */
+		@Override
 		public final void handleEvent(final Event event) {
 			if (!enabled) {
 				return;
@@ -437,6 +438,7 @@ public class KeyBindingDispatcher {
 		final long myStartTime = startTime;
 		final Display display = getDisplay();
 		display.timerExec(DELAY, new Runnable() {
+			@Override
 			public void run() {
 				if ((System.currentTimeMillis() > (myStartTime - DELAY))
 						&& (startTime == myStartTime)) {
