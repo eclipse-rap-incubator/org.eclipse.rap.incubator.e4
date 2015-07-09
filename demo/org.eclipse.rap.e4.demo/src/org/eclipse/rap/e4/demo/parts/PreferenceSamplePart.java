de525f9e2906ddc195448292a806cd67987c1b21
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-07-09 18:13:33 +0200

    Prevent error on color conversion in preferences part
    Add listener on default selection to update preference
    on pressing enter.
    
    Set color on color label instead of parent to keep text
    readable.

--- demo/org.eclipse.rap.e4.demo/src/org/eclipse/rap/e4/demo/parts/PreferenceSamplePart.java
+++ demo/org.eclipse.rap.e4.demo/src/org/eclipse/rap/e4/demo/parts/PreferenceSamplePart.java
@@ -6,81 +6,86 @@ import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.di.extensions.Preference;
 import org.eclipse.rap.e4.preferences.EPreference;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.FocusAdapter;
-import org.eclipse.swt.events.FocusEvent;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 
-@SuppressWarnings("restriction")
+@SuppressWarnings({ "restriction", "serial" })
 public class PreferenceSamplePart {
-	
-	private Label value;
-	
+
+	private Label colorLabel;
 	private Color currentColor;
 
 	@Optional
 	@Inject
 	@Preference
 	EPreference prefs;
-	
+
 	@Inject
 	public PreferenceSamplePart(Composite parent) {
 		parent.setLayout(new GridLayout(2, false));
-		
-		Label l = new Label(parent, SWT.NONE);
-		l.setText("Current Value:");
-		
-		value = new Label(parent, SWT.NONE);
-		value.setText("-");
-		
-		l = new Label(parent, SWT.NONE);
-		l.setText("Color");
-		
+
+		Label l1 = new Label(parent, SWT.NONE);
+		l1.setText("Current Value:");
+
+		colorLabel = new Label(parent, SWT.NONE);
+		colorLabel.setText("-");
+
+		Label l2 = new Label(parent, SWT.NONE);
+		l2.setText("Color:");
+
 		final Text t = new Text(parent, SWT.BORDER);
-		t.addFocusListener(new FocusAdapter() {
+		Listener listener = new Listener() {
 			@Override
-			public void focusLost(FocusEvent event) {
+			public void handleEvent(Event event) {
 				prefs.setString("colorPref", t.getText());
 			}
-		});
+		};
+		t.addListener(SWT.DefaultSelection, listener);
+		t.addListener(SWT.FocusOut, listener);
 		t.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 	}
-	
+
 	@Inject
 	public void updatePreference(@Optional @Preference("colorPref") String v) {
-		if( v == null ) {
+		if (v == null) {
 			return;
 		}
-		value.setText(v);
-		Color c = new Color(value.getDisplay(), toRGB(v));
-		value.getParent().setBackground(c);
-		
-		if( currentColor != null ) {
-			currentColor.dispose();	
+		colorLabel.setText(v);
+		RGB rgb = toRGB(v);
+		Color c = rgb != null ? new Color(colorLabel.getDisplay(), rgb) : null;
+		colorLabel.setBackground(c);
+		if (currentColor != null) {
+			currentColor.dispose();
 		}
 		currentColor = c;
-		
-		value.getParent().layout(true);
+
+		colorLabel.getParent().layout(true);
 	}
-	
-	private RGB toRGB(String hexcolor) {
-		String r;
-		String g;
-		String b;
-		if( hexcolor.length() == 3 ) {
-			r = hexcolor.substring(0, 1)+hexcolor.substring(0, 1);
-			g = hexcolor.substring(1, 2)+hexcolor.substring(1, 2);
-			b = hexcolor.substring(2, 3)+hexcolor.substring(2, 3);
-		} else {
-			r = hexcolor.substring(0, 2);
-			g = hexcolor.substring(2, 4);
-			b = hexcolor.substring(4, 6);
+
+	private static RGB toRGB(String hexcolor) {
+		try {
+			if (hexcolor.length() == 3) {
+				int r = Integer.parseInt(hexcolor.substring(0, 1), 16) * 17;
+				int g = Integer.parseInt(hexcolor.substring(1, 2), 16) * 17;
+				int b = Integer.parseInt(hexcolor.substring(2, 3), 16) * 17;
+				return new RGB(r, g, b);
+			}
+			if (hexcolor.length() == 6) {
+				int r = Integer.parseInt(hexcolor.substring(0, 2), 16);
+				int g = Integer.parseInt(hexcolor.substring(2, 4), 16);
+				int b = Integer.parseInt(hexcolor.substring(4, 6), 16);
+				return new RGB(r, g, b);
+			}
+		} catch (NumberFormatException e) {
+			// return fall back
 		}
-		return new RGB(Integer.parseInt(r, 16),Integer.parseInt(g, 16),Integer.parseInt(b, 16));
+		return null;
 	}
 }
